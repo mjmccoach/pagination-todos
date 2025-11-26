8158e941edc4755155e53c0fd56a4d71837e8fdd
Matthew <m.mccoach88@gmail.com>
2025-11-26 18:43:10 +0000

    WIP.

--- pagination.js
+++ pagination.js
@@ -1,12 +1,26 @@
 let total = 0;
 let skip = 0;
+let todos = [];
 
 const getToDos = function (skip) {
     fetch(`https://dummyjson.com/todos?limit=20&skip=${skip}`)
     .then(res => res.json())
-    .then(data => () => {
-            total = data.total;
-            skip = skip + 20
-            todos = data.todos
-        });
-}
+    .then(data => 
+        console.log(data.todos),
+        incrementSkip()
+        );
+};
+
+incrementSkip = function() {
+    skip = skip + 20;
+};
+
+getToDos(skip);
+console.log("Skip is ", skip);
+console.log("Todos are ", todos.map((todo) => {console.log(todo)}));
+getToDos(skip);
+console.log("Skip is ", skip);
+console.log("Todos are ", todos.map((todo) => todo.id));
+getToDos(skip);
+console.log("Skip is ", skip);
+console.log("Todos are ", todos.map((todo) => todo.id));
